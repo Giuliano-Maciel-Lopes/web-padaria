bfebee5a608e5c6f37eef688455338f393da720b
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-06-27 17:36:11 -0300

    test: antes de add api

---
+++ src/components/bakeryshowcase/corrosel.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  img: string;
+  img2: string;
+  name: string;
+};
+
+export function Carrossel({ img, img2, name }: Props) {
+  return (
+    <div className="w-full h-17 bg-gradient-animated flex flex-col  p-4 my-20">
+      <div className="flex items-center  w-full h-full justify-between">
+        <img
+          src={img2}
+          alt={`${name} imagem secundária`}
+          className="h-full w-24 rounded object-cover "
+        />
+        <p className="text-lg text-white font-semibold  ">{name}</p>
+        <img
+          src={img}
+          alt={`${name} imagem principal`}
+          className="h-full w-24 rounded object-cover "
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/bakery.tsx
+++ src/pages/bakery.tsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import { Buttoncategory } from "../components/bakeryshowcase/category";
 import { Slid } from "../components/bakeryshowcase/slider";
 import { ProductsView } from "../components/layouts/products";
+import { Carrossel } from "../components/bakeryshowcase/corrosel";
+
+import doce1 from "../assets/rosquinhas - Copia.png"
+
+
 
 export function Bakery() {
-  const [activecat, setActiveCat] = useState<null| string>(null);
+  const [activecat, setActiveCat] = useState<null | string>(null);
 
   const categories = [
     "Pães",
@@ -19,17 +24,18 @@ export function Bakery() {
     "Congelados",
   ];
   const produtos = [
-  { id: "1", nome: "Pão Francês", preco: 1.20 },
-  { id: "2", nome: "Bolo de Chocolate", preco: 6.50 },
-  { id: "3", nome: "Torta de Frutas", preco: 5.00 },
-  { id: "4", nome: "Café Expresso", preco: 3.00 },
-  { id: "5", nome: "Coxinha", preco: 4.00 },
-  { id: "6", nome: "Bolo de Fubá", preco: 5.50 },
-];
+    { id: "1", nome: "Pão Francês", preco: 1.2 },
+    { id: "2", nome: "Bolo de Chocolate", preco: 6.5 },
+    { id: "3", nome: "Torta de Frutas", preco: 5.0 },
+    { id: "4", nome: "Café Expresso", preco: 3.0 },
+    { id: "5", nome: "Coxinha", preco: 4.0 },
+    { id: "6", nome: "Bolo de Fubá", preco: 5.5 },
+  ];
 
   return (
     <div className="flex flex-col">
       <Slid />
+
       <div className="flex gap-4  my-5 md:my-10 overflow-x-auto scroll-smooth md:px-8 hide-scrollbar">
         {categories.map((cat) => (
           <Buttoncategory
@@ -40,9 +46,17 @@ export function Bakery() {
           />
         ))}
       </div>
+
       <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {produtos.map((products)=> <ProductsView name={products.nome.toUpperCase()} value={products.preco} />)}
+        {produtos.map((products) => (
+          <ProductsView
+            name={products.nome.toUpperCase()}
+            value={products.preco}
+          />
+        ))}
       </div>
+    <Carrossel img={doce1} img2={doce1} name="PRA ADOÇAR SEU DIA !"/>
+
     </div>
   );
 }

e36d495c212ed0263e9a3808553dfbe03bdf39d8
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-07-23 14:44:29 -0300

    feat:react query configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { GlobalAuthModals } from "./components/global/globalAuth";
 import { AsideAuthProvider } from "./context/asideauth-context";
 import { AuthProvider } from "./context/auth-context";
@@ -5,18 +7,24 @@ import { CartProvider } from "./context/cart-context";
 import { UserInfoProvider } from "./context/useinfo-context";
 
 import { Routes } from "./routes";
+import { query } from "./services/reactquery";
 
 export function App() {
   return (
-    <AuthProvider>
-      <UserInfoProvider>
-        <CartProvider>
-          <AsideAuthProvider>
-            <Routes />
-            <GlobalAuthModals />
-          </AsideAuthProvider>
-        </CartProvider>
-      </UserInfoProvider>
-    </AuthProvider>
+    <QueryClientProvider client={query}>
+      <AuthProvider>
+        <UserInfoProvider>
+          <CartProvider>
+            <AsideAuthProvider>
+              <Routes />
+              <GlobalAuthModals />
+            </AsideAuthProvider>
+          </CartProvider>
+        </UserInfoProvider>
+      </AuthProvider>
+
+      
+      {import.meta.env.MODE === "development" && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   );
 }
